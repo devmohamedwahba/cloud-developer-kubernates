f7ba88bb230dc12c37f43f84e46bca1b58acb523
Mohamed__Wahba <Mohamed.w@trufla.com>
2021-09-14 22:58:00 +0200

    first commit

---
+++ secrets.exmaple.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+export POSTGRESS_USERNAME=udegramdev
+export POSTGRESS_PASSWORD=udegramdev
+export POSTGRESS_DB=udegramdev
+export POSTGRESS_HOST=udegramdev.c5pdit2sola2.us-east-1.rds.amazonaws.com
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export AWS_BUCKET=arn:aws:s3:us-east-1:582920855356:accesspoint/test
+export JWT_SECRET=testing
+export url=http://localhost:8100
